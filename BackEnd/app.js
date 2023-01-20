6b0b6bb9bda0410bf6757871497ff1824580c435
AugustoBarcelos <augustoebo@taginfraestrutura.com.br>
2023-01-20 17:23:44 -0300

    updated backend

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,42 +1,44 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+mongoose.set("strictQuery", false);
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
   }
@@ -44,17 +46,20 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred! - Please check the console' });
+  res.json({
+    message:
+      error.message || "An unknown error occurred! - Please check the console",
+  });
 });
 
+const MONGODB_URI = `mongodb+srv://aebo92:123@cluster0.ag2n7gi.mongodb.net/mern?retryWrites=true&w=majority`;
+
 mongoose
-  .connect(
-    `mongodb+srv://aebo92:123@cluster0.ag2n7gi.mongodb.net/${process.env.DB_NAME}`
-  )
+  .connect(MONGODB_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000);
     console.log("Database is online");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
